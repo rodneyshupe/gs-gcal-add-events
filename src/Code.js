189681718285a8025fde5a10bc0bc4eeb1e62ef7
rodneyshupe <rodney@shupe.ca>
2023-02-04 10:45:11 -0800

    Add whitecaps games

--- src/Code.js
+++ src/Code.js
@@ -6,8 +6,13 @@ const BuildBCLionsCalendar = () => {
 };
 
 const BuildWhitecapsCalendar = () => {
-    const calendarId = "186vqua8r0v6e3ctr7764pqvpo@group.calendar.google.com";  //getCalendarIdByName("Vancouver Whitecaps FC");
+    const calendarId = "186vqua8r0v6e3ctr7764pqvpo@group.calendar.google.com"; //getCalendarIdByName("Vancouver Whitecaps FC");
     const events = readCalendarEvents(calendarId, new Date("01/01/2023"), new Date("12/01/2023"));
 
     Logger.log(JSON.stringify(events, null, 2));
 };
+
+const AddWhitecapsEvents = () => {
+    const calendarId = getCalendarIdByName("Whitecaps Tickets");
+    createCalendarEvents(whitecapsHome2023, calendarId);
+}
