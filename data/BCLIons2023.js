0e1c8136faf22a9ed2026a4411adb3587f76ddf0
rodneyshupe <rodney@shupe.ca>
2023-02-04 10:43:38 -0800

    BC Lions Schedule

---
+++ data/BCLIons2023.js
@@ -0,0 +1,201 @@
+const BCLions2023 = [{
+        "summary": "BC Lions Away Game - Preseason",
+        "start": "2023-05-27"
+    },
+    {
+        "summary": "Lions at Riders - Preseason",
+        "location": "Mosaic Stadium, 1700 Elphinstone St, Regina, SK S4P 2Z6, Canada",
+        "start": "2023-05-27T16:00:00-07:00",
+        "end": "2023-05-27T19:00:00-07:00"
+    },
+    {
+        "summary": "BC Lions Home Game - Preseason",
+        "start": "2023-06-01"
+    },
+    {
+        "summary": "Lions vs. Stamps - Preseason",
+        "location": "BC Place, 777 Pacific Blvd, Vancouver, BC V6B 4Y8, Canada",
+        "start": "2023-06-01T19:30:00-07:00",
+        "end": "2023-06-01T22:30:00-07:00"
+    },
+    {
+        "summary": "BC Lions Away Game - Stamps",
+        "start": "2023-06-08"
+    },
+    {
+        "summary": "Lions at Stamps",
+        "location": "McMahon Stadium, Calgary, AB, Canada",
+        "start": "2023-06-08T18:00:00-07:00",
+        "end": "2023-06-08T21:00:00-07:00"
+    },
+    {
+        "summary": "BC Lions Home Game - Elks",
+        "start": "2023-06-17"
+    },
+    {
+        "summary": "Lions vs. Elks",
+        "location": "BC Place, 777 Pacific Blvd, Vancouver, BC V6B 4Y8, Canada",
+        "start": "2023-06-17T16:00:00-07:00",
+        "end": "2023-06-17T19:00:00-07:00"
+    },
+    {
+        "summary": "BC Lions Away Game - Bombers",
+        "start": "2023-06-22"
+    },
+    {
+        "summary": "Lions at Bombers",
+        "location": "IG Field, 315 Chancellor Matheson Rd, Winnipeg, MB R3T 1Z2, Canada",
+        "start": "2023-06-22T17:30:00-07:00",
+        "end": "2023-06-22T20:30:00-07:00"
+    },
+    {
+        "summary": "BC Lions Away Game - Argos",
+        "start": "2023-07-03"
+    },
+    {
+        "summary": "Lions at Argos",
+        "location": "BMO Field, 170 Princes' Blvd, Toronto, ON M6K 3C3, Canada",
+        "start": "2023-07-03T16:00:00-07:00",
+        "end": "2023-07-03T19:00:00-07:00"
+    },
+    {
+        "summary": "BC Lions Home Game - Alouettes",
+        "start": "2023-07-09"
+    },
+    {
+        "summary": "Lions vs. Alouettes",
+        "location": "BC Place, 777 Pacific Blvd, Vancouver, BC V6B 4Y8, Canada",
+        "start": "2023-07-09T16:00:00-07:00",
+        "end": "2023-07-09T19:00:00-07:00"
+    },
+    {
+        "summary": "BC Lions Home Game - Riders",
+        "start": "2023-07-22"
+    },
+    {
+        "summary": "Lions vs. Riders",
+        "location": "BC Place, 777 Pacific Blvd, Vancouver, BC V6B 4Y8, Canada",
+        "start": "2023-07-22T16:00:00-07:00",
+        "end": "2023-07-22T19:00:00-07:00"
+    },
+    {
+        "summary": "BC Lions Away Game - Elks",
+        "start": "2023-07-29"
+    },
+    {
+        "summary": "Lions at Elks",
+        "location": "Commonwealth Stadium, 11000 Stadium Rd, Edmonton, AB T5H 4E2, Canada",
+        "start": "2023-07-29T16:00:00-07:00",
+        "end": "2023-07-29T19:00:00-07:00"
+    },
+    {
+        "summary": "BC Lions Away Game - Bombers",
+        "start": "2023-08-03"
+    },
+    {
+        "summary": "Lions at Bombers",
+        "location": "IG Field, 315 Chancellor Matheson Rd, Winnipeg, MB R3T 1Z2, Canada",
+        "start": "2023-08-03T17:30:00-07:00",
+        "end": "2023-08-03T20:30:00-07:00"
+    },
+    {
+        "summary": "BC Lions Home Game - Stamps",
+        "start": "2023-08-12"
+    },
+    {
+        "summary": "Lions vs. Stamps",
+        "location": "BC Place, 777 Pacific Blvd, Vancouver, BC V6B 4Y8, Canada",
+        "start": "2023-08-12T16:00:00-07:00",
+        "end": "2023-08-12T19:00:00-07:00"
+    },
+    {
+        "summary": "BC Lions Away Game - Riders",
+        "start": "2023-08-20"
+    },
+    {
+        "summary": "Lions at Riders",
+        "location": "Mosaic Stadium, 1700 Elphinstone St, Regina, SK S4P 2Z6, Canada",
+        "start": "2023-08-20T16:00:00-07:00",
+        "end": "2023-08-20T19:00:00-07:00"
+    },
+    {
+        "summary": "BC Lions Home Game - Ticats",
+        "start": "2023-08-26"
+    },
+    {
+        "summary": "Lions vs. Ticats",
+        "location": "BC Place, 777 Pacific Blvd, Vancouver, BC V6B 4Y8, Canada",
+        "start": "2023-08-26T16:00:00-07:00",
+        "end": "2023-08-26T19:00:00-07:00"
+    },
+    {
+        "summary": "BC Lions Away Game - Alouettes",
+        "start": "2023-09-02"
+    },
+    {
+        "summary": "Lions at Alouettes",
+        "location": "Percival Molson Memorial Stadium, 475 Av. des Pins, Montréal, QC H2W 1S4, Canada",
+        "start": "2023-09-02T16:00:00-07:00",
+        "end": "2023-09-02T19:00:00-07:00"
+    },
+    {
+        "summary": "BC Lions Home Game - RedBlacks",
+        "start": "2023-09-16"
+    },
+    {
+        "summary": "Lions vs. RedBlacks",
+        "location": "BC Place, 777 Pacific Blvd, Vancouver, BC V6B 4Y8, Canada",
+        "start": "2023-09-16T16:00:00-07:00",
+        "end": "2023-09-16T19:00:00-07:00"
+    },
+    {
+        "summary": "BC Lions Away Game - Elks",
+        "start": "2023-09-22"
+    },
+    {
+        "summary": "Lions at Elks",
+        "location": "Commonwealth Stadium, 11000 Stadium Rd, Edmonton, AB T5H 4E2, Canada",
+        "start": "2023-09-22T18:30:00-07:00",
+        "end": "2023-09-22T21:30:00-07:00"
+    },
+    {
+        "summary": "BC Lions Home Game - Riders",
+        "start": "2023-09-29"
+    },
+    {
+        "summary": "Lions vs. Riders",
+        "location": "BC Place, 777 Pacific Blvd, Vancouver, BC V6B 4Y8, Canada",
+        "start": "2023-09-29T19:30:00-07:00",
+        "end": "2023-09-29T22:30:00-07:00"
+    },
+    {
+        "summary": "BC Lions Home Game - Bombers",
+        "start": "2023-10-06"
+    },
+    {
+        "summary": "Lions vs. Bombers",
+        "location": "BC Place, 777 Pacific Blvd, Vancouver, BC V6B 4Y8, Canada",
+        "start": "2023-10-06T19:00:00-07:00",
+        "end": "2023-10-06T22:00:00-07:00"
+    },
+    {
+        "summary": "BC Lions Away Game - Ticats",
+        "start": "2023-10-13"
+    },
+    {
+        "summary": "Lions at Ticats",
+        "location": "Tim Hortons Field, 64 Melrose Ave N, Hamilton, ON L8L 8C1, Canada",
+        "start": "2023-10-13T16:00:00-07:00",
+        "end": "2023-10-13T19:00:00-07:00"
+    },
+    {
+        "summary": "BC Lions Home Game - Stamps",
+        "start": "2023-10-20"
+    },
+    {
+        "summary": "Lions vs. Stamps",
+        "location": "BC Place, 777 Pacific Blvd, Vancouver, BC V6B 4Y8, Canada",
+        "start": "2023-10-20T19:00:00-07:00",
+        "end": "2023-10-20T22:00:00-07:00"
+    }
+]
